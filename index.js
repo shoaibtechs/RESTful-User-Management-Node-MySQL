df1f6366f59142f50e1c406a3e825b9cdcda10ac
shoaibtechs <p200147@pwr.nu.edu.pk>
2026-01-21 17:27:23 +0500

    feat: implement faker data generation and bul insert into mysql

--- index.js
+++ index.js
@@ -2,12 +2,89 @@ const express =  require("express");
 const app = express();
 const port = 8080;
 
+const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+
+
 app.listen(port, ()=>{
 
      console.log(`The server is listening at port ${port}`);
 
 
 })
+
+
+
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'registry_system_db',
+  password : "Shoaib098890@"
+});
+
+
+
+const createRandomUser = () => {
+  return [
+    
+    faker.string.uuid(),
+    faker.internet.username(),
+    faker.internet.email(),
+    faker.internet.password(),
+    
+    ]; 
+
+}
+
+
+
+let data = [];
+
+for(let i=1; i<=100; i++)
+{
+    data.push(createRandomUser());
+
+
+}
+
+console.log("Data Count ", data.length);
+
+
+
+let q =  "INSERT into users(id, username, email, password) values ?  ";
+
+// let q =  "select * from users";
+
+
+
+
+try{
+
+    connection.query(q, [data], (err, result)=>{
+
+        if (err) throw err;
+        console.log(result);
+
+
+
+    })
+
+
+
+}
+
+
+
+catch(err){
+
+    console.log(err);
+
+}
+
+
+
 
 
 app.get("/", (req, res)=>{
